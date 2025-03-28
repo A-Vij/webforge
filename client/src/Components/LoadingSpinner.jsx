4be06179606990f299818bdaf00a231b32e3bf41
Arastu Vij <arastuvij@Arastus-MacBook-Pro.local>
2025-03-28 13:36:05 +0530

    updated spinner

--- client/src/Components/LoadingSpinner.jsx
+++ client/src/Components/LoadingSpinner.jsx
@@ -2,10 +2,10 @@ import { motion } from "framer-motion";
 
 const LoadingSpinner = () => {
 	return (
-		<div className='min-h-screen bg-gradient-to-br from-black via-purple-900 to-indigo-900 flex items-center justify-center relative overflow-hidden'>
+		<div className="min-h-screen flex items-center justify-center">
 			{/* Stylish Loading Spinner */}
 			<motion.div
-				className='w-16 h-16 border-4 border-t-4 border-t-indigo-500 border-indigo-300 rounded-full shadow-lg'
+				className="w-16 h-16 border-4 border-t-4 border-t-indigo-500 border-indigo-300 rounded-full shadow-lg"
 				animate={{ rotate: 360 }}
 				transition={{ duration: 1.2, repeat: Infinity, ease: "linear" }}
 			/>
